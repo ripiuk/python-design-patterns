abb4ad8d859df231b8a12b7f9f3eea2b904eba27
Oleksandr <oleksandrr@fornova.net>
2019-07-10 00:18:16 +0300

    Added factory method and abstract factory

---
+++ abstract_factory/abstract_factory.py
@@ -0,0 +1,161 @@
+"""
+Абстрактна фабрика — це породжуючий патерн проектування,
+який вирішує проблему створення цілих сімейств пов’язаних продуктів,
+без прив’язки коду до конкретних класів продуктів.
+
+Абстрактна фабрика задає інтерфейс створення всіх доступних типів продуктів,
+а кожна конкретна реалізація фабрики породжує продукти однієї з варіацій.
+Клієнтський код викликає методи фабрики для отримання продуктів,
+замість самостійного створювання їх за допомогою оператора new.
+При цьому, фабрика сама стежить за тим, щоб створюваний продукт був потрібної варіації.
+
+Переваги:
+    Гарантує поєднання створюваних продуктів.
+    Звільняє клієнтський код від прив’язки до конкретних класів продукту.
+    Виділяє код виробництва продуктів в одне місце, спрощуючи підтримку коду.
+    Спрощує додавання нових продуктів до програми.
+    Реалізує принцип відкритості/закритості.
+Недоліки:
+    Ускладнює код програми внаслідок введення великої кількості додаткових класів.
+    Вимагає наявності всіх типів продукту в кожній варіації.
+
+Абстрактная фабрика  — это устройство классов, облегчающее создание семейств продуктов.
+Что такое семейство продуктов? Например, классы Транспорт + Двигатель + Управление.
+Вариациями этого семейства могут стать:
+    Автомобиль + ДвигательВнутренннегоСгорания + Руль
+    Самолет + РеактивныйДвигатель + Штурвал
+Если у вас нет семейств продуктов, значит не может быть и абстрактной фабрики.
+"""
+import platform
+from abc import ABC, abstractmethod
+
+
+# ====== Buttons ======
+
+class Button(ABC):
+
+    @abstractmethod
+    def design(self) -> None:
+        pass
+
+
+class WinButton(Button):
+
+    def design(self):
+        print("Here are some instructions of how to draw windows button")
+
+
+class LinuxButton(Button):
+
+    def design(self):
+        print("Here are some instructions of how to draw linux button")
+
+
+class MacButton(Button):
+
+    def design(self):
+        print("Here are some instructions of how to draw mac button")
+
+
+# ====== Text Areas ======
+
+class TextArea(ABC):
+
+    @abstractmethod
+    def design(self):
+        pass
+
+
+class WinTextArea(TextArea):
+
+    def design(self):
+        print("Here are some instructions of how to draw windows text area")
+
+
+class LinuxTextArea(TextArea):
+
+    def design(self):
+        print("Here are some instructions of how to draw linux text area")
+
+
+class MacTextArea(TextArea):
+
+    def design(self):
+        print("Here are some instructions of how to draw mac text area")
+
+
+# ====== Factories ======
+
+class GUIFactory(ABC):
+
+    @abstractmethod
+    def create_button(self) -> Button:
+        pass
+
+    @abstractmethod
+    def create_text_area(self) -> TextArea:
+        pass
+
+
+class WinFactory(GUIFactory):
+
+    def create_button(self) -> WinButton:
+        print("Creating windows button")
+        return WinButton()
+
+    def create_text_area(self) -> WinTextArea:
+        print("Creating windows text area element")
+        return WinTextArea()
+
+
+class LinuxFactory(GUIFactory):
+
+    def create_button(self) -> LinuxButton:
+        print("Creating linux button")
+        return LinuxButton()
+
+    def create_text_area(self) -> LinuxTextArea:
+        print("Creating linux text area element")
+        return LinuxTextArea()
+
+
+class MacFactory(GUIFactory):
+
+    def create_button(self) -> MacButton:
+        print("Creating linux button")
+        return MacButton()
+
+    def create_text_area(self) -> MacTextArea:
+        print("Creating linux text area element")
+        return MacTextArea()
+
+
+# ====== Application ======
+
+class Application:
+
+    def __init__(self, factory: GUIFactory):
+        self.button = factory.create_button()
+        self.text_area = factory.create_text_area()
+
+    def draw(self):
+        self.button.design()
+        self.text_area.design()
+
+
+if __name__ == "__main__":
+    platform_factories = {
+        "Linux": LinuxFactory,
+        "Windows": WinFactory,
+        "MacOS": MacFactory,
+    }
+    my_os = platform.system()
+
+    os_factory = platform_factories.get(my_os)
+    if not os_factory:
+        Exception("Unknown operation system")
+
+    os_factory = os_factory()
+
+    app = Application(os_factory)
+    app.draw()
---
+++ factory_method/factory_method.py
@@ -0,0 +1,119 @@
+"""
+Фабричний метод — це породжуючий патерн проектування,
+який вирішує проблему створення різних продуктів,
+без прив’язки коду до конкретних класів продуктів.
+
+Фабричний метод задає метод, який необхідно використовувати замість виклику оператора
+new для створення об’єктів-продуктів. Підкласи можуть перевизначити цей метод,
+щоб змінювати тип створюваних продуктів.
+
+Переваги:
+    Позбавляє клас від прив’язки до конкретних класів продуктів.
+    Виділяє код виробництва продуктів в одне місце, спрощуючи підтримку коду.
+    Спрощує додавання нових продуктів до програми.
+    Реалізує принцип відкритості/закритості.
+Недоліки:
+    Може призвести до створення великих паралельних ієрархій класів,
+        адже для кожного класу продукту потрібно створити власний підклас творця.
+
+Паттерн Фабричный метод  — это устройство классов, при котором подклассы могут
+переопределять тип создаваемого в суперклассе продукта. Если вы имеете иерархию продуктов
+и абстрактный создающий метод, который переопределяется в подклассах,
+то перед вами паттерн Фабричный метод.
+"""
+import platform
+from abc import ABC, abstractmethod
+
+
+# ====== Buttons ======
+
+class Button:
+
+    @property
+    @abstractmethod
+    def height(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def weight(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def color(self) -> str:
+        pass
+
+
+class WinButton(Button):
+
+    @property
+    def height(self) -> int:
+        return 10
+
+    @property
+    def weight(self) -> int:
+        return 20
+
+    @property
+    def color(self) -> str:
+        return "Blue"
+
+
+class LinuxButton(Button):
+
+    @property
+    def height(self) -> int:
+        return 15
+
+    @property
+    def weight(self) -> int:
+        return 25
+
+    @property
+    def color(self) -> str:
+        return "Grey"
+
+
+# ====== Dialogs ======
+
+class Dialog(ABC):
+
+    @abstractmethod
+    def create_button(self) -> Button:
+        pass
+
+    def draw(self) -> None:
+        button = self.create_button()
+        print("Drawing button. Height: {}, Weight: {}, Color: {}".format(
+            button.height, button.weight, button.color
+        ))
+
+
+class WinDialog(Dialog):
+
+    def create_button(self) -> WinButton:
+        return WinButton()
+
+
+class LinuxDialog(Dialog):
+
+    def create_button(self) -> LinuxButton:
+        return LinuxButton()
+
+
+# ====== Application ======
+
+if __name__ == "__main__":
+    platform_factories = {
+        "Linux": LinuxDialog,
+        "Windows": WinDialog,
+    }
+    my_os = platform.system()
+
+    dialog = platform_factories.get(my_os)
+    if not dialog:
+        Exception("Unknown operation system")
+
+    dialog = dialog()
+    dialog.draw()
